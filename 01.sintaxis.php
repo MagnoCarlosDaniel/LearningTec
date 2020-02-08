0f8bd323909b7491cbd091584e84f11c7999d311
CarlosMagno <carloscmasmas@gmail.com>
2020-02-08 11:08:40 -0600

    como se manejan variables

--- 01.sintaxis.php
+++ 01.sintaxis.php
@@ -1,5 +1,4 @@
 <?php
 print "mi primer codigo";
-
-
+echo "<p>mi segundo hola mundo</p>","<h3>mi tercer hola mundo</h3>";/*muestra mas de un resultado o es de concatenacion*/
 ?>
---
+++ 02.variables.php
@@ -0,0 +1,15 @@
+<?php
+#variable numerica
+print "<h1>Aprendiendo a hacer variables</h1>";
+$numero = 5;/*las variables deben de tener un signo de pesos empezando*/
+echo "<numero es = $numero";
+echo "<br><br>";
+#variable texto
+$palabra = "soy carlos";
+echo "como te llamas : hola $palabra";
+echo "<br><br>";
+#variable booleana
+$boleanito = true;
+echo "mi valor booleano inicial es $boleanito"
+
+?>
